ab8763d2999a35ad593a009d40843449ffc31842
Actionb <yummytea1@gmail.com>
2023-02-22 14:01:15 +0100

    Trim test settings

--- tests/settings.py
+++ tests/settings.py
@@ -11,30 +11,10 @@ INSTALLED_APPS = [
 
 ROOT_URLCONF = "tests.urls"
 
-MIDDLEWARE = (
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",  # required for django.contrib.admin
-    "django.contrib.messages.middleware.MessageMiddleware",  # required for django.contrib.admin
-)
-
-STATIC_URL = "/static/"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.request",
-                "django.template.context_processors.debug",
-                "django.template.context_processors.i18n",
-                "django.template.context_processors.media",
-                "django.template.context_processors.static",
-                "django.template.context_processors.tz",
-                "django.contrib.messages.context_processors.messages",
-            ]
-        },
     }
 ]
 
